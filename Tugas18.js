f22ff2dfd3cf5198e39c8818803a43c59ccab539
T R A $ H <sulthons.a19@gmail.com>
2023-10-05 15:56:29 +0700

    Tugas 18 JavaScript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ Tugas18.js
@@ -0,0 +1,12 @@
+function Tugas18() {
+  var kalimat_Tugas18 =
+    " it is a long established fact that a reader be distracted by the readable content of a page when looking at its layout";
+  console.log(kalimat_Tugas18.slice(9, 26));
+  var kalimat2_Tugas18 =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+  var kalimat3_Tugas18 = kalimat2_Tugas18.concat(
+    "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham."
+  );
+  console.log(kalimat3_Tugas18);
+}
+Tugas18();
